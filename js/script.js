cf1a3dc6ac0b01b6f78429b95a9c6bf84c4096bb
Katarzyna Migacz <k.migacz94@gmail.com>
2022-11-29 19:09:20 +0100

    improve js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
-console.log("Saluton!");
+{
+    const welcome = () => {
+        console.log("Saluton!");
+    };
 
-let button = document.querySelector(".js-button");
-let container = document.querySelector(".js-container");
-let mainHeader = document.querySelector(".js-mainHeader");
-let darkOrBright = document.querySelector(".js-darkOrBright");
+    const button = document.querySelector(".js-button");
 
-button.addEventListener("click", () => {
-    container.classList.toggle("container--darkMotive");
-    button.classList.toggle("header__button--dark");
-    mainHeader.classList.toggle("header__mainHeader--dark");
-    darkOrBright.innerText = container.classList.contains("container--darkMotive") ? "jasny" : "ciemny";
-});
+    const onChangeMotiveClick = () => {
+        const container = document.querySelector(".js-container");
+        const mainHeader = document.querySelector(".js-mainHeader");
+        const darkOrBright = document.querySelector(".js-darkOrBright");
+
+        container.classList.toggle("container--darkMotive");
+        button.classList.toggle("header__button--dark");
+        mainHeader.classList.toggle("header__mainHeader--dark");
+        darkOrBright.innerText = container.classList.contains("container--darkMotive") ? "jasny" : "ciemny";
+    };
+
+    const init = () => {
+        button.addEventListener("click", onChangeMotiveClick);
+        welcome();
+    };
+    
+    init();
+}
